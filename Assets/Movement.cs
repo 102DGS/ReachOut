d6daae07efb7f977c8759875a19d4c63d4ebf3ec
JayKimyra <KiraKimyra@gmail.com>
2020-10-18 18:05:32 +0300

    11

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        run();
+        
+    }
+    void run()
+    {
+        _rigidbody.velocity = Vector2.left * Input.GetAxis("Horizontal");
+    }
+}
+ 
